5f6266614a148b81291b1008e31752ed021a5e34
george <georgevenven@gmail.com>
2023-02-07 09:41:16 -0800

    basic arch laid out

---
+++ source/generator.js
@@ -0,0 +1,3 @@
+// Simple Process 
+// 1. Go through all the files in articles, select the ones that are valid (have frontmatter)
+// 2. Convert each of them to valid html, and insert them into the generated folder 
